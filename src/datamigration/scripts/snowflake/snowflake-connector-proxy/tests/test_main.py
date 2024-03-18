f64f50a3bbedfa514cdc1c3af7a61ea5981c20aa
Sumit Banerjee <sumitba@google.com>
2024-03-18 15:09:39 +0530

    add snowflake-connector-proxy code

---
+++ src/datamigration/scripts/snowflake/snowflake-connector-proxy/tests/test_main.py
@@ -0,0 +1,22 @@
+import pytest
+
+import main
+
+
+@pytest.fixture
+def client():
+    main.APP.testing = True
+    return main.APP.test_client()
+
+
+def test_handler_home(client):
+    r = client.get("/")
+    assert r.status_code == 404
+
+
+def test_handler_migrate_data(client):
+    r = client.get("/migrate-data")
+    assert r.status_code == 405
+
+    r = client.post("/migrate-data")
+    assert r.status_code == 400
