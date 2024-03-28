e2aa9a9523da94b5f7e31686792212be621e8c0a
Sumit Banerjee <sumitba@google.com>
2024-03-29 01:42:02 +0530

    add ./extract-ddl route to snowflake connector proxy

--- src/datamigration/scripts/snowflake/snowflake-connector-proxy/main.py
+++ src/datamigration/scripts/snowflake/snowflake-connector-proxy/main.py
@@ -135,5 +135,77 @@ def migrate_data():
         return {"msg": str(e)}, HTTPStatus.BAD_REQUEST
 
 
+@APP.route("/extract-ddl", methods=["POST"])
+def extract_ddl():
+    """
+    Setup and start snowflake connector jar, call extract-ddl api of the jar, and finally terminate the jar
+    """
+    try:
+        params = request.get_json()
+        setup_config = params["setup_config"]
+        oauth_config = params["oauth_config"]
+        translation_config = params["translation_config"]
+
+        port = get_open_port()
+        sf_connector = SfConnectorUtils(_SF_CONNECTOR_HOST, port)
+        run_dir = f"{_RUNS_BASE_DIR}/{port}"
+        os.makedirs(run_dir, exist_ok=True)
+
+        request_body_template_vars = {
+            "warehouse": setup_config["warehouse"],
+            "storageIntegration": setup_config["storageIntegration"],
+            "database": translation_config["sourceDatabaseName"],
+            "schema": translation_config["sourceSchemaName"],
+        }
+        request_body_template = _TEMPLATE_ENV.get_template(_REQUEST_BODY_TEMPLATE_FILE)
+        request_body_output = request_body_template.render(request_body_template_vars)
+        request_body_file_path = os.path.join(run_dir, "snowflake_request_body.json")
+        with open(request_body_file_path, "w") as f:
+            f.write(request_body_output)
+
+        query_mapping_file_path = os.path.join(
+            run_dir, "snowflake_table_query_mapping.json"
+        )
+        with open(query_mapping_file_path, "w") as f:
+            if (
+                "table_query_mapping" in setup_config
+                and setup_config["table_query_mapping"]
+            ):
+                f.write(setup_config["table_query_mapping"])
+            else:
+                f.write("{}")
+
+        app_properties_template_vars = {
+            "jdbc_url": setup_config["jdbcUrl"],
+            "account_url": setup_config["accountUrl"],
+            "request_body_file": request_body_file_path,
+            "query_mapping_file": query_mapping_file_path,
+        }
+        app_properties_template = _TEMPLATE_ENV.get_template(
+            _APP_PROPERTIES_TEMPLATE_FILE
+        )
+        app_properties_output = app_properties_template.render(
+            app_properties_template_vars
+        )
+        app_properties_file_path = os.path.join(run_dir, "application.properties")
+        with open(app_properties_file_path, "w") as f:
+            f.write(app_properties_output)
+
+        jar_process = start(app_properties_file_path, port)
+        try:
+            sf_connector.save_oauth_values(
+                client_id=oauth_config["clientId"],
+                client_secret=oauth_config["clientSecret"],
+                refresh_token=oauth_config["refreshToken"],
+            )
+            res = sf_connector.extract_ddl(translation_config)
+        finally:
+            logging.info("Stopping snowflake connector jar")
+            jar_process.kill()
+        return res, HTTPStatus.OK
+    except Exception as e:
+        return {"msg": str(e)}, HTTPStatus.BAD_REQUEST
+
+
 if __name__ == "__main__":
     APP.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
